0cd65bfe9ef81ff4d9334243199435aec92bd1a3
James Newling <james.newling@gmail.com>
2024-01-22 08:54:10 -0800

    docs improvement, only print removal lines if #lines decreases

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-# mlir-diff-tooling
+## mlir-diff-tooling
 
 Very basic tools for seeing how IR changes as it gets lowered through passes. 
+
+
+Example:
+
+Create a file with some IR, such as `examples/parallel_reductions.mlir`.
+
+Run the following command to see the IR after each pass, dumped to a file. 
+
+
+```
+ ~/iree-build/tools/iree-compile --iree-hal-target-backends=amd-aie --mlir-print-ir-after-all --mlir-print-ir-module-scope  --mlir-disable-threading examples/parallel_reductions.mlir > ir_after_all.mlir 2>&1
+```
+
+
+Generate using this tool:
+
+```
+python3 mlir_diff_tooling.py examples/parallel_reductions.mlir ir_after_all.mlir > diff.html
+```
+
+
 
 
--- ir_diff_generator.py
+++ ir_diff_generator.py
@@ -39,6 +39,8 @@ def getStringDiff(str1_lines, str2_lines):
     diff = [x for x in diff if not x.startswith("?")]
     newDiff = []
 
+    decrease = len(str1_lines) >= len(str2_lines)
+
     def uninterestingChange(i, targetStart):
         l = diff[i]
         for k in [1, -1]:
@@ -56,7 +58,7 @@ def getStringDiff(str1_lines, str2_lines):
         l = diff[i]
         if l.startswith("-"):
             # only print removal lines if they're interesting.
-            if not uninterestingChange(i, "+"):
+            if decrease and not uninterestingChange(i, "+"):
                 newDiff.append(fillMinus(l))
 
         elif l.startswith("+"):
@@ -84,8 +86,11 @@ def main(input_ir_fn, after_all_fn):
     for l in f0.readlines():
         bodies[-1].append(l)
 
+    
     with open(after_all_fn, "r", encoding="utf-8", errors="ignore") as f0:
+
         for l in f0:
+
             try:
                 if "IR Dump" in l:
                     heads.append(l)
@@ -95,27 +100,36 @@ def main(input_ir_fn, after_all_fn):
 
             except UnicodeDecodeError:
                 break  # Stop iterating over the lines on error
-
+    
+    
     assert len(heads) == len(bodies)
 
     final = []
     final.append(heads[0])
     final.extend(bodies[0])
 
+    nLines = 0
     nChanged = 0
     lastBody = bodies[0]
     for i in range(1, len(heads)):
         if bodies[i] != lastBody:
+            nChanged += 1
             final.append("\n\n")
+            final.append(heads[i])
             final.append(
                 "// line count " + str(len(lastBody)) + " -> " + str(len(bodies[i]))
             )
             final.append("\n")
-            final.append(heads[i])
-            diff = getStringDiff(lastBody, bodies[i])
-            final.extend(diff)
-            nChanged += 1
+
+            if nLines < 10000:
+                diff = getStringDiff(lastBody, bodies[i])
+                final.extend(diff)
+            else:
+                final.append("// skipping diff because line count is too high\n")
+
+
             lastBody = bodies[i]
+            nLines += len(lastBody)
 
     print(
         "// Number of passes that changed the IR: ",
